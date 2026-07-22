2d66987df1677d9d5e6435a889987c60bc8070f7
dishacharpe376 <dishacharpe376@gmail.com>
2026-07-22 18:41:13 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,31 @@
+# Task 2 - Python Basics
+
+# Variables
+name = "Disha"
+age = 19
+
+# Data Types
+print("Name:", name)
+print("Age:", age)
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+
+# Operators
+a = 20
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# Loop
+print("\nNumbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
+
+# Function
+def square(num):
+    return num * num
+
+print("\nSquare of 6:", square(6))
